f5f304bd7a23194d6d6dec7adb0c1d04287d9329
Ahmed Eldeeb <eldeeb757575@gmail.com>
2024-06-19 05:52:37 +0300

    Implemented list_all() function to list all documents

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -1,1 +1,19 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+""" NoSQL Project """
+
+
+def list_all(mongo_collection):
+    """
+    list all documents in a collection
+
+    Args:
+        mongo_collection: pymongo collection object
+
+    Returns:
+        Empty list if no document in the collection
+        Otherwise, a list of documents in the collection
+    """
+    if mongo_collection is None:
+        return []
+
+    return list(mongo_collection.find())
