74f27dee885d1a159dd5f886f3ff75f6cd45f01c
Ahmed Eldeeb <eldeeb757575@gmail.com>
2024-06-20 15:47:32 +0300

    Update update_topics function documentation and shebang line

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -1,1 +1,17 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+""" NoSQL Project """
+
+
+def update_topics(mongo_collection, name, topics):
+    """
+    Function that changes all topics of a school document based on the name
+
+    Args:
+        mongo_collection: pymongo collection object
+        name: (string) the school name to update
+        topics: (list of strings) the list of topics approached in the school
+
+    Returns:
+        Nothing
+    """
+    mongo_collection.update_many({"name": name}, {"$set": {"topics": topics}})
