918a2f2dfa5d4394750cca58c298bd1a067cc73f
Ahmed Eldeeb <eldeeb757575@gmail.com>
2024-06-20 15:51:35 +0300

    Update schools_by_topic function documentation and shebang line

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -1,1 +1,16 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+""" NoSQL Project """
+
+
+def schools_by_topic(mongo_collection, topic):
+    """
+    returns the list of school having a specific topic
+
+    Args:
+        mongo_collection: the pymongo collection object
+        topic: topic searched
+
+    Returns:
+        list of schools having a specific topic
+    """
+    return mongo_collection.find({"topics": topic})
