1c80fc368e9707f6c17b97befb3ba15a35ed0411
Ahmed Eldeeb <eldeeb757575@gmail.com>
2024-06-20 15:42:27 +0300

    Update insert_school function documentation and shebang line

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -1,1 +1,16 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+""" NoSQL Project """
+
+
+def insert_school(mongo_collection, **kwargs):
+    """
+    inserts a new document in a collection based on kwargs
+
+    Args:
+        mongo_collection: the pymongo collection object
+        kwargs: key-value pairs to be inserted in the document
+
+    Returns:
+        the new _id
+    """
+    return mongo_collection.insert_one(kwargs).inserted_id
