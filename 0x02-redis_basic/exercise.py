822c47313bb7a8e37f1d04d1f08b22965c52caa6
Ahmed Eldeeb <eldeeb757575@gmail.com>
2024-07-03 19:02:52 +0300

    Add call_history decorator to Cache class

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -20,6 +20,22 @@ def count_calls(method: Callable) -> Callable:
     return wrapper
 
 
+def call_history(method: Callable) -> Callable:
+    """
+    Decorator to store the history of inputs and outputs for a method
+    """
+    key = method.__qualname__
+
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        """ Wrapper function """
+        self._redis.rpush(f"{key}:inputs", str(args))
+        output = method(self, *args, **kwargs)
+        self._redis.rpush(f"{key}:outputs", output)
+        return output
+    return wrapper
+
+
 class Cache():
     """ Cache Class """
     def __init__(self):
@@ -28,6 +44,7 @@ class Cache():
         self._redis.flushdb()
 
     @count_calls
+    @call_history
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """
         Store data in cache and return a key
