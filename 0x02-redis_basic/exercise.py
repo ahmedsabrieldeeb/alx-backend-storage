04398b17ca9044abf06f27bac727a110858dc6ea
Ahmed Eldeeb <eldeeb757575@gmail.com>
2024-07-03 18:11:40 +0300

    Add get, get_str, and get_int methods to Cache class

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -2,7 +2,7 @@
 """ Exercise Redis """
 import redis
 import uuid
-from typing import Union
+from typing import Union, Callable
 
 
 class Cache():
@@ -13,7 +13,56 @@ class Cache():
         self._redis.flushdb()
 
     def store(self, data: Union[str, bytes, int, float]) -> str:
-        """ Store data in cache and return a key """
+        """
+        Store data in cache and return a key
+
+        Args:
+            data: Data to store
+
+        Returns:
+            key: Key to access the data
+        """
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Callable = None) -> \
+            Union[str, bytes, int, float]:
+        """
+        Get data from cache
+
+        Args:
+            key: Key to get its value
+            fn (=None): Function to convert the data
+
+        Returns:
+            data: Converted data
+        """
+        data = self._redis.get(key)
+        if fn:
+            return fn(data)
+        return data
+
+    def get_str(self, key: str) -> str:
+        """
+        Get data from cache as string
+
+        Args:
+            key: Key to get its value
+
+        Returns:
+            data: Data as string
+        """
+        return self.get(key, fn=lambda d: d.decode("utf-8"))
+
+    def get_int(self, key: str) -> int:
+        """
+        Get data from cache as integer
+
+        Args:
+            key: Key to get its value
+
+        Returns:
+            data: Data as integer
+        """
+        return self.get(key, fn=int)
