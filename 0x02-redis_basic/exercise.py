b400360d738f756a9f817b1214b0b5ac78ff5e57
Ahmed Eldeeb <eldeeb757575@gmail.com>
2024-07-03 15:52:08 +0300

    Add Cache class to exercise.py

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,1 +1,19 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+""" Exercise Redis """
+import redis
+import uuid
+from typing import Union
+
+
+class Cache():
+    """ Cache Class """
+    def __init__(self):
+        """ Constructor to setup the cache """
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        """ Store data in cache and return a key """
+        key = str(uuid.uuid4())
+        self._redis.set(key, data)
+        return key
