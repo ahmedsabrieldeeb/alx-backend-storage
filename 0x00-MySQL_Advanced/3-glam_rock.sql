3be60aded0a33fdf71fe49782d48a469b4b7df42
Ahmed Eldeeb <eldeeb757575@gmail.com>
2024-08-22 15:38:39 +0300

    Add SQL script to list bands with Glam rock as their main style, ranked by longevity

---
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,8 @@
+-- Write a SQL script that lists all bands with Glam rock
+-- as their main style, ranked by their longevity
+-- You should use attributes formed and split for computing the lifespan
+
+SELECT band_name, (IFNULL(split, '2022') - formed) AS lifespan
+FROM metal_bands
+WHERE style LIKE '%Glam rock%'
+ORDER BY lifespan DESC;
