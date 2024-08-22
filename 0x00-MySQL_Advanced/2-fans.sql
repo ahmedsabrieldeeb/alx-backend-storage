21fd03eb4ea059bae4c0218cb194f6be070dde9d
Ahmed Eldeeb <eldeeb757575@gmail.com>
2024-08-22 15:18:23 +0300

    Refactor SQL query to calculate the total number of fans by summing the 'fans' column for each origin

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -2,7 +2,7 @@
 -- ordered by the number of (non-unique) fans
 -- column names must be: origin and nb_fans
 
-SELECT origin, COUNT(*) AS nb_fans
+SELECT origin, SUM(fans) AS nb_fans
 FROM metal_bands
 GROUP BY origin
 ORDER BY nb_fans DESC;
