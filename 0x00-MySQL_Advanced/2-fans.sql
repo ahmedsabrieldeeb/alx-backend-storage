994a73c24708fe81142bdb850cbe345a8b1cac1a
Ahmed Eldeeb <eldeeb757575@gmail.com>
2024-08-22 15:15:03 +0300

    Add SQL script to rank country origins of metal bands by number of fans

---
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -0,0 +1,8 @@
+-- script that ranks country origins of bands,
+-- ordered by the number of (non-unique) fans
+-- column names must be: origin and nb_fans
+
+SELECT origin, COUNT(*) AS nb_fans
+FROM metal_bands
+GROUP BY origin
+ORDER BY nb_fans DESC;
